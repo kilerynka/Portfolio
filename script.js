95fa23f34315fd2aa190166812f901b3206cbec4
Jakub Zakrzewicz <hipiseczek@gmail.com>
2021-02-17 20:18:08 +0100

    add icons and animated on scroll in javascript

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const home = document.querySelector(".home");
 const about = document.querySelector(".about");
+const skills = document.querySelector(".skills");
+
+
+const htmlIcon = document.querySelector(".html");
+const cssIcon = document.querySelector(".css");
+const gitIcon = document.querySelector(".git");
+const sassIcon = document.querySelector(".sass");
+
+
+
+let windowHeight = window.innerHeight;
+
+
+
+
+function animationStart() {
+ let scrollFromTop = window.pageYOffset;
+ console.log(scrollFromTop)  
+
+ if(scrollFromTop > 1100) {
+  htmlIcon.style.animation = "html .5s linear 1s 1 alternate forwards";
+  cssIcon.style.animation = "css .5s linear 1s 1 alternate forwards";
+ }
+
+ if(scrollFromTop > 1700) {
+  gitIcon.style.animation = "git .5s linear 1s 1 alternate forwards";
+  sassIcon.style.animation = "sass .5s linear 1s 1 alternate forwards";
+ }
+}
+
+
+
+
+window.addEventListener("scroll", animationStart)
 
 home.addEventListener("click", () => {
   window.scrollTo({
@@ -10,7 +44,16 @@ home.addEventListener("click", () => {
 
 about.addEventListener("click", () => {
   window.scrollTo({
-    top: 1009,
+    top: windowHeight,
     behavior: "smooth",
   });
 });
+
+skills.addEventListener("click", () => {
+  window.scrollTo({
+    top: windowHeight * 2,
+    behavior: "smooth",
+  });
+});
+
+ 
